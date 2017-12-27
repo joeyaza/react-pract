c7c11dfce16139b8eb26adfbd27e7533b8d2e859
jazadehdel <jazadehdel@sovrn.com>
2017-12-27 12:29:13 +0000

    first set of components added

---
+++ src/components/Header.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+class Header extends React.Component {
+
+    render() {
+
+        return (
+
+            <p>Header</p>
+
+
+        )
+
+    }
+
+}
+
+export default Header;
