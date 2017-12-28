dab343ce70f3164a4195c46ab0421a2359080a21
jazadehdel <jazadehdel@sovrn.com>
2017-12-28 21:25:22 +0000

    fish saved using state

---
+++ src/components/AddFishForm.js
@@ -0,0 +1,46 @@
+import React from "react";
+
+class AddFishForm extends React.Component {
+
+    createFish(event) {
+
+        event.preventDefault();
+
+        const fish = {
+            name: this.name.value,
+            price: this.price.value,
+            status: this.status.value,
+            desc: this.desc.value,
+            image: this.image.value
+        };
+
+        this.props.addFish(fish);
+        this.fishForm.reset();
+
+    }
+
+    render() {
+
+        return (
+
+           <form ref={(input) => this.fishForm = input} className="fish-edit" onSubmit={(e) => this.createFish(e)}>
+
+               <input ref={(input)=> this.name = input} type="text" placeholder="Name" />
+               <input ref={(input)=> this.price = input} type="text" placeholder="Price" />
+               <select ref={(input)=> this.status = input}>
+                   <option value="available">Fresh!</option>
+                   <option value="unavailable">All Gone!</option>
+               </select>
+               <textarea ref={(input)=> this.desc = input} placeholder="Desc"></textarea>
+               <input ref={(input)=> this.image = input} type="text" placeholder="Image" />
+               <button type="submit">+ Add Item</button>
+           </form>
+
+
+        )
+
+    }
+
+}
+
+export default AddFishForm;
