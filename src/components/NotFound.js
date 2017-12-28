c9204546cf4e1b495a328b5f2ca40d89972dca69
jazadehdel <jazadehdel@sovrn.com>
2017-12-28 20:00:37 +0000

    routing to storepicker sorted with reactRouter

---
+++ src/components/NotFound.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+class NotFound extends React.Component {
+
+    render() {
+
+        return (
+
+            <p>NotFound!!!!!!</p>
+
+
+        )
+
+    }
+
+}
+
+export default NotFound;
