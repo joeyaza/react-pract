ec3909511098c1040d62c1ed9f463dfccb692f76
jazadehdel <jazadehdel@sovrn.com>
2018-01-12 11:10:41 +0000

    adding fish to order using state/ props and making total

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,9 @@ import React from "react";
 import Header from "./Header";
 import Order from "./Order";
 import Inventory from "./Inventory";
+import Fish from "./Fish";
+import sampleFishes from "../sample-fishes";
+
 
 class App extends React.Component {
 
@@ -10,6 +13,8 @@ class App extends React.Component {
         super();
 
         this.addFish = this.addFish.bind(this);
+        this.loadSamples = this.loadSamples.bind(this);
+        this.addToOrder = this.addToOrder.bind(this);
 
         this.state = {
 
@@ -17,6 +22,23 @@ class App extends React.Component {
             order: {}
 
         };
+
+    }
+
+    loadSamples() {
+
+        this.setState({
+            fishes: sampleFishes
+        })
+
+    }
+
+    addToOrder(key) {
+
+        const order = {...this.state.order};
+        order[key] = order[key] + 1 || 1;
+        this.setState({order});
+
 
     }
 
@@ -37,9 +59,15 @@ class App extends React.Component {
             <div className="catch-of-the-day">
                 <div className="menu">
                     <Header tagline="Fresh Seafood, come and enjoy!" />
+                    <ul className="list-of-fishes">
+                        {
+                            Object.keys(this.state.fishes)
+                                .map(key => <Fish key={key} index={key} details={this.state.fishes[key]} addToOrder={ this.addToOrder }/>)
+                        }
+                    </ul>
                 </div>
-                <Order />
-                <Inventory addFish={this.addFish} />
+                <Order fishes={this.state.fishes} order={this.state.order} />
+                <Inventory addFish={this.addFish} loadSamples={this.loadSamples} />
             </div>
 
         )
--- src/helpers.js
+++ src/helpers.js
@@ -1,6 +1,6 @@
 export function formatPrice(cents) {
 
-  return `$${(cents / 100).toFixed(2).replace(/\B(?=(\d{3})+(?!\d))/g, ',')}`;
+  return `£${(cents / 100).toFixed(2).replace(/\B(?=(\d{3})+(?!\d))/g, ',')}`;
 
 }
 
